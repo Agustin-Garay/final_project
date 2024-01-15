524ae4b410fd37ea5d6b87db7eaf46b2724d15a7
Agustin-Garay <155218253+Agustin-Garay@users.noreply.github.com>
2024-01-15 02:46:54 -0300

    Update emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -6,4 +6,12 @@ def emotion_detector(text_to_analyse):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = input, headers = headers)
-    return json.loads(response.text)
+    response_emotion_predictor = json.loads(response.text)["emotionPredictions"]["emotion"]
+    score_max = 0
+    emotion_max = None
+    for emotion, score in response_emotion_predictor
+        if score > score_max
+            score_max = score
+            emotion_max = emotion
+    respose_emotion_predictor["dominant_emotion"] = emotion_max
+    return response_emotion_predictor
