d356b737d1c822d6a610eeba103fd966fe4ffe92
Agustin-Garay <155218253+Agustin-Garay@users.noreply.github.com>
2024-01-15 17:49:19 -0300

    Update server.py

--- server.py
+++ server.py
@@ -11,12 +11,12 @@ def render_index_page():
 def emo_detector():
     text_to_analyse = request.args.get("textToAnalyze")
     response = emotion_detector(text_to_analyse)
-    anger = response["anger"]
-    disgust = response["disgust"]
-    fear = response["fear"]
-    joy = response["joy"]
-    sadness = response["sadness"]
-    dominant = response["dominant_emotion"]
+    anger = str(response["anger"])
+    disgust = str(response["disgust"])
+    fear = str(response["fear"])
+    joy = str(response["joy"])
+    sadness = str(response["sadness"])
+    dominant = str(response["dominant_emotion"])
     return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.".format(anger, disgust, fear, joy, sadness, dominant)
 
 if __name__ == "__main__":
