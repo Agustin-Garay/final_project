d23ef59956fd060622f9abea8268752eb98af15b
Agustin-Garay <155218253+Agustin-Garay@users.noreply.github.com>
2024-01-16 13:32:25 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+"""Application Server"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,13 +7,15 @@ app = Flask("emotionDetector")
 
 @app.route("/")
 def render_index_page():
+    """Renders the main page"""
     return render_template('index.html')
 
 @app.route("/emotionDetector")
 def emo_detector():
+    """Runs the emotion detector once the button is clicked"""
     text_to_analyse = request.args.get("textToAnalyze")
     response = emotion_detector(text_to_analyse)
-    if response["dominant_emotion"] == None:
+    if response["dominant_emotion"] is None:
         return "Invalid text! Please try again!"
     anger = str(response["anger"])
     disgust = str(response["disgust"])
@@ -19,7 +23,9 @@ def emo_detector():
     joy = str(response["joy"])
     sadness = str(response["sadness"])
     dominant = str(response["dominant_emotion"])
-    return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.".format(anger, disgust, fear, joy, sadness, dominant)
+    return f"For the given statement, the system response is 'anger': {anger}, \
+        'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. \
+        The dominant emotion is {dominant}."
 
 if __name__ == "__main__":
     app.run(port=5000)
